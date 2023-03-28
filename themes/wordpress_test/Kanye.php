dab4e17be7eb565784c1f65b50ef485115e31958
Waleed Anjum <waleedanjum@gmail.com>
2023-03-28 23:21:00 +0500

    code optimized

--- themes/wordpress_test/Kanye.php
+++ themes/wordpress_test/Kanye.php
@@ -1,29 +1,21 @@
 <?php get_header() ?>
 <?php /* Template Name: Kanye Posts */ ?>
-
-
 <?php
     $url = 'https://api.kanye.rest/';
     $quotes = [];
-    
     for ($i = 0; $i < 5; $i++) {
         $response = file_get_contents($url);
         $data = json_decode($response, true);
         $quotes[] = $data['quote'];
     }
 ?>
-
-
     <h1>Here are 5 Kanye quotes:</h1>
     <ul>
         <?php foreach ($quotes as $quote): ?>
             <li><?php echo $quote; ?></li>
         <?php endforeach; ?>
     </ul>
-
-
     <?php 
-    
     function hs_give_me_coffee() {
         $url = 'http://localhost/wordpress_test_project/wp-json';
         $response = wp_remote_get( $url );
@@ -34,10 +26,5 @@
         $coffee_data = json_decode( $body );
         return $coffee_data->file;
     }
-    
-    
-    
     ?>
-
-
 <?php get_footer() ?>
